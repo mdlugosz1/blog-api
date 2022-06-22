303e5483b5e338e11c79aecf5094643cc5c3e80e
mdlugosz1 <mdugosz@gmail.com>
2022-06-22 21:04:19 +0200

    Add cors and post route

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv").config();
 require("./config/auth");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
+const postsRouter = require("./routes/posts");
 
 const app = express();
 
@@ -19,6 +21,7 @@ mongoose.connect(process.env.DATABASE_CREDENTIALS, {
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MonogoDB connection error:"));
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -27,6 +30,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/posts", postsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
