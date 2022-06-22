b34b1064c1b314d63f78f246bb8ebd801dfe6aeb
mdlugosz1 <mdugosz@gmail.com>
2022-06-22 21:03:51 +0200

    Add ref to author in post model, default date

--- models/post.js
+++ models/post.js
@@ -3,10 +3,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
-	author: { type: String },
+	author: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
 	title: { type: String, required: true },
 	content: { type: String, required: true },
-	date: { type: Date },
+	date: { type: Date, default: new Date() },
 	published: Boolean,
 	comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
 });
